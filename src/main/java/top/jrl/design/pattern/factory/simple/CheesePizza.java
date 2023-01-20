aa66096561a380917ac2f2aa346fb76e95b16b55
Joly163 <18990341730@163.com>
2023-01-20 13:56:39 +0800

    简单工厂

---
+++ src/main/java/top/jrl/design/pattern/factory/simple/CheesePizza.java
@@ -0,0 +1,12 @@
+package top.jrl.design.pattern.factory.simple;
+
+/**
+ * 具体产品类
+ * @author jrl
+ * @date Create in 13:49 2023/1/20
+ */
+public class CheesePizza extends Pizza {
+    public CheesePizza(){
+        this.name = "CheesePizza";
+    }
+}
---
+++ src/main/java/top/jrl/design/pattern/factory/simple/PizzaStore.java
@@ -0,0 +1,22 @@
+package top.jrl.design.pattern.factory.simple;
+
+/**
+ * @author jrl
+ * @date Create in 13:39 2023/1/20
+ */
+public class PizzaStore {
+    SimplePizzaFactory simplePizzaFactory;
+
+    public PizzaStore(SimplePizzaFactory simplePizzaFactory) {
+        this.simplePizzaFactory = simplePizzaFactory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = simplePizzaFactory.createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
